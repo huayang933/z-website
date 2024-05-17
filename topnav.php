00232a88b950297266e22f70edfdab9e1c0aab9a
huayang933 <139694294+huayang933@users.noreply.github.com>
2024-05-17 14:35:40 +0800

    update top nav

---
+++ topnav.php
@@ -0,0 +1,44 @@
+<!-- Header Start -->
+<header class="transparent d-flex align-items-center justify-content-between w-100" id="metrix-scrollspy">
+    <!-- Navbar Start -->
+    <nav class="navbar navbar-expand-lg w-100" id="mainNav">
+        <div class="container">
+            <a class="brand" href="home.html">
+                <img src="assets/images/logo/logo.svg" alt="logo" />
+            </a>
+            <button class="navbar-toggler toggler ms-auto" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="before-menu"><i class="far fa-bars"></i></span>
+                <span class="after-menu"><i class="fal fa-times"></i></span>
+            </button>
+            <div class="collapse navbar-collapse position-lg-relative justify-content-end" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+
+                    <!-- <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownPages" role="button"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Pages<i data-icon="arrowDown"></i>
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownPages">
+                            <a class="dropdown-item drop-link" href="about.html">Abouts Us</a>
+                            <a class="dropdown-item drop-link" href="blog.html">Blog</a>
+                        </div>
+                    </li> -->
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="/">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="/about">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="/contact">Contact</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="/portfolio">Portfolio</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+</header>
