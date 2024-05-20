fc1b60c5439f5346ebf64a6af3b99b1822c6f762
huayang933 <139694294+huayang933@users.noreply.github.com>
2024-05-20 17:40:28 +0800

    update footer

--- footer.php
+++ footer.php
@@ -1,72 +1,72 @@
-	<!-- Footer Start -->
-	<footer class="footer-one">
-		<div class="container">
-			<div class="footer-style-one">
-				<div class="row">
-					<!-- Footer Logo & Text -->
-					<div class="col-6 offset-md-3 col-md-3 offset-lg-0 col-lg-2 order-2 order-lg-1">
-						<div class="footer-one-link-group text-lg-start">
-							<ul class="footer-one-link-list">
-								<li class="footer-one-link-head">Zeta Capital Sdn Bhd</li>
-								<li><a href="/" class="footer-one-link">Home</a></li>
-								<li><a href="/services" class="footer-one-link">Services</a></li>
-								<li><a href="/about" class="footer-one-link">About</a></li>
-								<li><a href="/portfolio" class="footer-one-link">Portfolio</a></li>
-								<li><a href="/contact" class="footer-one-link">Contact us</a></li>
-							</ul>
-						</div>
-					</div>
-					<!-- Footer Logo & Text -->
-					<!-- Footer App Download Link -->
-					<div class="col-lg-8 order-1 order-lg-2">
-						<div class="footer-one-content-wrapper">
-							<a class="footer-one-brand" href="home.html">
-								<img src="assets/images/logo/logo-white.svg" alt="logo" />
-							</a>
-							<p class="footer-one-text">Intrinsicly matrix high standards in niches whereas
-								intermandated niche markets. Objectively harness competitive resources.</p>
-
-							<!-- Social Links -->
-							<div class="footer-one-social-links-group">
-								<a href="https://www.facebook.com/" target="_blank" class="footer-one-social-link">
-									<i data-icon="facebook"></i>
-								</a>
-								<a href="https://www.instagram.com/" target="_blank" class="footer-one-social-link">
-									<i data-icon="instagram"></i>
-								</a>
-								<a href="https://www.pinterest.com/" target="_blank" class="footer-one-social-link">
-									<i data-icon="pinterest"></i>
-								</a>
-								<a href="https://twitter.com/" target="_blank" class="footer-one-social-link">
-									<i data-icon="x"></i>
-								</a>
-							</div>
-
-
-						</div>
-					</div>
-					<!-- Footer App Download Link -->
-				</div>
-			</div>
-		</div>
-		<div class="col-12">
+<!-- Footer Start -->
+<footer class="footer-one">
+	<div class="container">
+		<div class="footer-style-one">
 			<div class="row">
-				<!-- Copyright Text -->
-				<div class="footer-one-copyright">
-					<label class="m-0">Copyright <strong>&COPY;</strong> 2024. All Rights Reserved.</label>
+				<!-- Footer Logo & Text -->
+				<div class="col-6 offset-md-3 col-md-3 offset-lg-0 col-lg-2 order-2 order-lg-1">
+					<div class="footer-one-link-group text-lg-start">
+						<ul class="footer-one-link-list">
+							<li class="footer-one-link-head">Zeta Capital Sdn Bhd</li>
+							<li><a href="/" class="footer-one-link">Home</a></li>
+							<li><a href="/services" class="footer-one-link">Services</a></li>
+							<li><a href="/about" class="footer-one-link">About</a></li>
+							<li><a href="/portfolio" class="footer-one-link">Portfolio</a></li>
+							<li><a href="/contact" class="footer-one-link">Contact us</a></li>
+						</ul>
+					</div>
 				</div>
-				<!-- Copyright Text -->
+				<!-- Footer Logo & Text -->
+				<!-- Footer App Download Link -->
+				<div class="col-lg-8 order-1 order-lg-2">
+					<div class="footer-one-content-wrapper">
+						<a class="footer-one-brand" href="home.html">
+							<img src="assets/images/logo/logo-white.svg" alt="logo" />
+						</a>
+						<p class="footer-one-text">Intrinsicly matrix high standards in niches whereas
+							intermandated niche markets. Objectively harness competitive resources.</p>
+
+						<!-- Social Links -->
+						<div class="footer-one-social-links-group">
+							<a href="https://www.facebook.com/" target="_blank" class="footer-one-social-link">
+								<i data-icon="facebook"></i>
+							</a>
+							<a href="https://www.instagram.com/" target="_blank" class="footer-one-social-link">
+								<i data-icon="instagram"></i>
+							</a>
+							<a href="https://www.pinterest.com/" target="_blank" class="footer-one-social-link">
+								<i data-icon="pinterest"></i>
+							</a>
+							<a href="https://twitter.com/" target="_blank" class="footer-one-social-link">
+								<i data-icon="x"></i>
+							</a>
+						</div>
+
+
+					</div>
+				</div>
+				<!-- Footer App Download Link -->
 			</div>
 		</div>
-	</footer>
+	</div>
+	<div class="col-12">
+		<div class="row">
+			<!-- Copyright Text -->
+			<div class="footer-one-copyright">
+				<label class="m-0">Copyright <strong>&COPY;</strong> 2024. All Rights Reserved.</label>
+			</div>
+			<!-- Copyright Text -->
+		</div>
+	</div>
+</footer>
 </div>
 
- <!-- Scroll to Top Start -->
- <a href="JavaScript:void(0)" class="toTop" onclick="toTopClick()">
-        <i data-icon="arrowUp"></i>
-    </a>
+<!-- Scroll to Top Start -->
+<a href="JavaScript:void(0)" class="toTop" onclick="toTopClick()">
+	<i data-icon="arrowUp"></i>
+</a>
 
-    <script src="https://unpkg.com/vue@2.6.14/dist/vue.js"></script>
+<script src="https://unpkg.com/vue@2.6.14/dist/vue.js"></script>
 <script src="https://unpkg.com/vue-router@3.5.2/dist/vue-router.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 <script>
@@ -114,7 +114,7 @@
 				this.portfolio = sheetTransformer(data.table.cols, data.table.rows);
 
 				setTimeout(() => {
-					
+
 				}, 1000);
 
 			}).catch((error) => {
@@ -142,12 +142,13 @@
 	});
 
 </script>
-    <!-- Jquery Javascript File -->
-    <script src="assets/js/jquery.min.js"></script>
-    <script src="assets/js/bootstrap.bundle.min.js"></script>
-    <script src="assets/js/owl.carousel.min.js"></script>
-    <script src="assets/js/smoothscroll.min.js"></script>
-    <script src="assets/js/svg.js"></script>
-    <script src="assets/js/custom.js"></script>
+<!-- Jquery Javascript File -->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/bootstrap.bundle.min.js"></script>
+<script src="assets/js/owl.carousel.min.js"></script>
+<script src="assets/js/smoothscroll.min.js"></script>
+<script src="assets/js/svg.js"></script>
+<script src="assets/js/custom.js"></script>
 </body>
+
 </html>
